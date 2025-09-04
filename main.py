a17b2c78a06aaefc6989b70a1dfc0b2655f13d14
vi <89683278+violetjordanhara@users.noreply.github.com>
2025-09-04 13:58:22 +0100

    added jisho api to main.py
    manually adding changes to github as laptop is broken

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from pytubefix import YouTube
 from pytubefix.cli import on_progress
 import whisper 
 import fugashi
+# from jisho_api import word
+from jisho_api.word import Word
 
 # url = "https://www.youtube.com/watch?v=tE9tSv6Lo-I"
 
@@ -12,12 +14,27 @@ import fugashi
 # ys.download()
 
 tagger = fugashi.Tagger()
-model = whisper.load_model("tiny")
-result = model.transcribe("「母、駅そばっ」第595話 | あたしンち | [ENG sub].m4a", language="japanese",fp16=False)
-# print(result["text"])
-words = tagger(result["text"])
-for word in words:
-    print(f"Surface: {word.surface}")
-    print(f"Features: {word.feature}")
-    print(f"Part of speech: {word.part_of_speech}")
+# model = whisper.load_model("tiny")
+# result = model.transcribe("「母、駅そばっ」第595話 | あたしンち | [ENG sub].m4a", language="japanese",fp16=False)
+# # print(result["text"])
+# fulltext = tagger(result["text"])
+
+testsentence = '昨日、東京は曇りでしたか。'
+tokenisedtext = tagger(testsentence)
+# print(tokenisedtext)
+
+for token in tokenisedtext:
+   
+    r = Word.request(token.surface)
+    print(r.data[0].senses[0].english_definitions)
+#     # print(f"Surface: {word.surface}")
+#     # print(f"Features: {word.feature.lemma}")
+#     r = word.request(text)
+#     print(r)
+    # if hasattr(word, 'part_of_speech'):
+    #     print(f"Part of speech: {word.part_of_speech}")
+    # else:
+    #     print("Part of speech: N/A")
+    # print("---")
 # looks through a video link you send it to create a transcript, then teases out useful vocab and grammar for everyday speech
+
