0edef9a429f3a9dc88d6b787a06ea35f3d1b7663
vi <89683278+violetjordanhara@users.noreply.github.com>
2025-09-05 15:26:55 +0100

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import fugashi
 # from jisho_api import word
 from jisho_api.word import Word
 
-# url = "https://www.youtube.com/watch?v=tE9tSv6Lo-I"
+# url = "https://www.youtube.com/watch?v=FWqxBg0N738"
 
 # yt = YouTube(url, on_progress_callback=on_progress)
 # print(yt.title)
@@ -14,23 +14,29 @@ from jisho_api.word import Word
 # ys.download()
 
 tagger = fugashi.Tagger()
-# model = whisper.load_model("tiny")
-# result = model.transcribe("「母、駅そばっ」第595話 | あたしンち | [ENG sub].m4a", language="japanese",fp16=False)
-# # print(result["text"])
-# fulltext = tagger(result["text"])
-
-testsentence = '昨日、東京は曇りでしたか。'
-tokenisedtext = tagger(testsentence)
-# print(tokenisedtext)
+model = whisper.load_model("tiny")
+result = model.transcribe("日本人がよく使う「遠慮」の使い方３つ.m4a", language="japanese",fp16=False)
+# print(result["text"])
+tokenisedtext = tagger(result["text"])
+# print(fulltext)
+# testsentence = '昨日、東京は曇りでしたか。'
+# tokenisedtext = tagger(fulltext)
+# # print(tokenisedtext)
 
 for token in tokenisedtext:
    pos = token.pos.split(',')[0]
    skip_pos = ['助動詞', '助詞', '記号', '補助記号']
-   r = Word.request(token.surface)
-   if token.surface not in '、。？！' and pos not in skip_pos:
-        print(token.surface,  r.data[0].senses[0].parts_of_speech, r.data[0].senses[0].english_definitions)
+   allowed_pos = ['動詞', '名詞']
+   
+   if token.surface in '、。？！' or pos not in allowed_pos:
+     print("skipped ", token.surface)
    else:
-        print(token.surface)
+          r = Word.request(token.surface)
+          if r and r.data:
+             print(token.pos,token.surface, r.data[0].japanese[0].reading, r.data[0].senses[0].parts_of_speech, r.data[0].senses[0].english_definitions)
+          else:
+              print('not found')
+
 
 #     # print(f"Surface: {word.surface}")
 #     # print(f"Features: {word.feature.lemma}")
@@ -42,4 +48,3 @@ for token in tokenisedtext:
     #     print("Part of speech: N/A")
     # print("---")
 # looks through a video link you send it to create a transcript, then teases out useful vocab and grammar for everyday speech
-
